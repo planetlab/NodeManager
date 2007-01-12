86f91f9fb04b9ecc122b08fcf443fe4ff01185f6
Mark Huang <mlhuang@cs.princeton.edu>
2007-01-12 00:07:37 +0000

    - fail with non-zero exit code on e.g. 404 errors

--- curlwrapper.py
+++ curlwrapper.py
@@ -4,7 +4,7 @@ from subprocess import PIPE, Popen
 class CurlException(Exception): pass
 
 def retrieve(url, cacert=None, postdata=None, timeout=300):
-    options = ('/usr/bin/curl',)
+    options = ('/usr/bin/curl', '--fail', '--silent')
     if cacert: options += ('--cacert', cacert)
     if postdata: options += ('--data', '@-')
     if timeout: options += ('--max-time', str(timeout))
