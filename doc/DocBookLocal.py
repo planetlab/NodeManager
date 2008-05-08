61955fee15191f1e01cd3003af1de4fbaef4c441
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2008-05-08 09:11:02 +0000

    first draft for the myplc-docs package (cross-module change)

--- doc/DocBookLocal.py
+++ doc/DocBookLocal.py
@@ -1,13 +1,20 @@
+#!/usr/bin/env python
 
+# PATHS to be set by the build system
+# this is in ..
 import api_calls
+# in PLCAPI/doc
+from DocBook import DocBook
 
-def get_func_list():
-	api_function_list = []
-	for func in dir(api_calls):
-		try:
-			f = api_calls.__getattribute__(func)
-			if 'group' in f.__dict__.keys():
-				api_function_list += [api_calls.__getattribute__(func)]
-		except:
-			pass
-	return api_function_list
+def api_methods():
+    api_function_list = []
+    for func in dir(api_calls):
+        try:
+            f = api_calls.__getattribute__(func)
+            if 'group' in f.__dict__.keys():
+                api_function_list += [api_calls.__getattribute__(func)]
+        except:
+            pass
+    return api_function_list
+
+DocBook(api_methods ()).Process()
