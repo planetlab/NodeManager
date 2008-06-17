d4df6a7c06677ea27339685007ea6b212b62461b
Faiyaz Ahmed <faiyaza@cs.princeton.edu>
2008-06-17 18:43:49 +0000

    5 minutes is too long of a timeout to keep an apache proc open on PLC.  90 seconds is probably more than enough.

--- plcapi.py
+++ plcapi.py
@@ -15,7 +15,7 @@ class PLCAPI:
     the new session-based method.
     """
 
-    def __init__(self, uri, cacert, auth, timeout = 300, **kwds):
+    def __init__(self, uri, cacert, auth, timeout = 90, **kwds):
         if isinstance(auth, (tuple, list)):
             (self.node_id, self.key) = auth
             self.session = None
