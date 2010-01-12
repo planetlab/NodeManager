781153c0786a2b6aea28ab88d53670f45182d890
Sapan Bhatia <sapanb@cs.princeton.edu>
2010-01-12 21:02:48 +0000

    Store the slice id of the current vserver on disk, rather than sticking it in the kernel.

--- sliver_vs.py
+++ sliver_vs.py
@@ -266,8 +266,9 @@ class Sliver_VS(accounts.Account, vserver.VServer):
             self.set_ipaddresses_config(self.rspec['ip_addresses'])
 
             if self.is_running():
-                logger.log("%s: Setting name to %s" % (self.name, self.slice_id),2)
-                self.setname(self.slice_id)
+                logger.log("%s: Storing slice id for PlanetFlow" % (self.name, self.slice_id),2)
+                #self.setname(self.slice_id)
+                file('/etc/vservers/%s/slice_id' % name, 'w').write(self.slice_id)
 
             if self.enabled == False:
                 self.enabled = True
