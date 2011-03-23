587e5a30120fef8a5065d5bdf8655c5042fb48f3
Thierry Parmentelat <thierry.parmentelat@sophia.inria.fr>
2011-03-23 15:19:28 +0100

    a yum_install function

--- sliver-initscripts/initscript-template.sh
+++ sliver-initscripts/initscript-template.sh
@@ -5,12 +5,27 @@ slicename=$1; shift
 # that can make sense if needed
 # source /etc/init.d/functions
 
+# a reasonably tested function for installing with yum
+function yum_install () {
+    pkg="$1"; shift
+    while true; do
+	rpm -q $pkg && break
+	sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY 
+	sudo yum -y install $pkg
+	sleep 10
+    done
+    echo $pkg installed
+}
+
+####################
 function start () {
     
 }
 function stop () {
 
 }
+
+####################
 function restart () {
   stop
   start
